dd90204f04aca2b5a05ab0b5022f1fbbffd7ca23
rsgamertv <Wersuser2@gmail.com>
2024-06-20 12:55:27 +0300

    last

--- lib/core/factors.dart
+++ lib/core/factors.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
 final defaultPadding = 3.w;
-final circlePadding = SizerUtil.deviceType == DeviceType.mobile ? 72.w : 80.w;
+final circlePadding = SizerUtil.deviceType == DeviceType.mobile ? 72.w : SizerUtil.orientation == Orientation.portrait ? 80.w : 91.w;
 final widgetMaxWidth = 70.w;
 const minHeight = 60;
 final maxHeight = 70.h;
-final circleHeight = SizerUtil.deviceType == DeviceType.mobile ? 100.h : 75.h;
+final circleHeight = SizerUtil.deviceType == DeviceType.mobile ? 100.h : SizerUtil.orientation == Orientation.portrait ? 100.h : 75.h;
 final circleWidth = 40.w;
