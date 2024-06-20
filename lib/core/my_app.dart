a32f8f020c2dd5b1f84d7a4f8d586e941dd84e48
rsgamertv <Wersuser2@gmail.com>
2024-06-20 13:01:05 +0300

    mobile systemChrome fixed

--- lib/core/my_app.dart
+++ lib/core/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dev_test_task/main/screen/home_page.dart';
 import 'package:sizer/sizer.dart';
 
@@ -8,6 +9,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var shortestSide = MediaQuery.of(context).size.shortestSide;
+    final bool isMobile = shortestSide < 600;
+    isMobile ?
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+    ]) :     
+    SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+    ]);
     return Sizer(
       builder: (context, orientation, deviceType) {
       return  const MaterialApp(
