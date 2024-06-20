16a27fd75b338f0c8331163f7f13989292749f5c
rsgamertv <Wersuser2@gmail.com>
2024-06-20 12:44:39 +0300

    commit

--- lib/core/factors.dart
+++ lib/core/factors.dart
@@ -1,7 +1,9 @@
 import 'package:sizer/sizer.dart';
 
-final defaultPadding = 5.w;
-final circlePadding = 80.w;
+final defaultPadding = 3.w;
+final circlePadding = SizerUtil.deviceType == DeviceType.mobile ? 70.w : 80.w;
 final widgetMaxWidth = 70.w;
 const minHeight = 60;
 final maxHeight = 70.h;
+final circleHeight = SizerUtil.deviceType == DeviceType.mobile ? 100.h : 75.h;
+final circleWidth = 40.w;
--- lib/widgets/circle_widget.dart
+++ lib/widgets/circle_widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dev_test_task/core/colors.dart';
-import 'package:sizer/sizer.dart';
+import 'package:flutter_dev_test_task/core/factors.dart';
 
 class CircleWidget extends StatelessWidget {
   const CircleWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 75.h,
-      width: 30.w,
-      decoration: const BoxDecoration(
-        color: primary,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(400),bottomLeft: Radius.circular(400))
+    return Center(
+      child: Container(
+        height: circleHeight,
+        width: circleWidth,
+        decoration: const BoxDecoration(
+          color: primary,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(400),bottomLeft: Radius.circular(400))
+        ),
       ),
     );
   }
